82fe8bbbe763b119dd2dfe5151e99f65f200d9b8
CarlMartins <lemos_oliveiraa@hotmail.com>
2022-09-24 17:26:05 -0300

    Aula 2 - Entendendo Tipos

--- OOP/ByteBank/ByteBank/ContaCorrente.cs
+++ OOP/ByteBank/ByteBank/ContaCorrente.cs
@@ -7,13 +7,16 @@ public class ContaCorrente
     public int NumeroAgencia { get; set; }
     public string NomeAgencia { get; set; }
     public double Saldo { get; set; }
+    public bool Verificador { get; set; }
 
-    public ContaCorrente(string titular, string conta, int numeroAgencia, string nomeAgencia, double saldo)
+    public ContaCorrente(string titular, string conta, int numeroAgencia, string nomeAgencia, double saldo,
+        bool verificador)
     {
         Titular = titular;
         Conta = conta;
         NumeroAgencia = numeroAgencia;
         NomeAgencia = nomeAgencia;
         Saldo = saldo;
+        Verificador = verificador;
     }
 }
--- OOP/ByteBank/ByteBank/Program.cs
+++ OOP/ByteBank/ByteBank/Program.cs
@@ -2,7 +2,8 @@
 
 Console.WriteLine("Boas vindas ao seu banco, ByteBank!");
 
-var conta1 = new ContaCorrente("Carlos Martins", "10123-X", 23, "Agência Central", 100.00);
+var conta1 = new ContaCorrente("Carlos Martins", "10123-X", 23, "Agência Central", 100.00, true);
+var conta2 = new ContaCorrente("Carlos Martins", "10123-X", 23, "Agência Central", 100.00, true);
 
 Console.WriteLine(
     $"Titular: {conta1.Titular} - " +
@@ -11,5 +12,14 @@ Console.WriteLine(
     $"Nome Agência: {conta1.NomeAgencia} - " +
     $"Saldo: {conta1.Saldo}");
 
+Console.WriteLine(
+    $"Titular: {conta2.Titular} - " +
+    $"Conta: {conta2.Conta} - " +
+    $"Numero Agência: {conta2.NumeroAgencia} - " +
+    $"Nome Agência: {conta2.NomeAgencia} - " +
+    $"Saldo: {conta2.Saldo}");
+
+
+Console.WriteLine(Equals(conta1, conta2));
 
 Console.ReadKey();
